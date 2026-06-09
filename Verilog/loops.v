413d0a582caf96cc676eafb8f6a2506acc0e42d7
Tanish <tanishgheewala@gmail.com>
2026-06-09 14:59:11 -0700

    Conditionals & Loops

---
+++ Verilog/loops.v
@@ -0,0 +1,30 @@
+// Conditional
+if statement begin
+    something;
+end
+else if statement begin
+    something;
+end
+else begin
+    something;
+end
+
+// While Loop
+while (condition) begin
+    something;
+end
+
+// Forever Loop
+forever begin
+    something;
+end
+
+// Repeat Loop
+repeat (num_iterations) begin
+    something;
+end
+
+// For Loop
+for (initialization; condition; increment) begin
+    something;
+end
